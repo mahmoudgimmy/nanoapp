681d66f9edaa77b25d9ef7422dfe36c09bf2acf2
mahmoudgimmy <mahmoud.gamal2791996@yahoo.com>
2016-11-26 19:51:57 +0200

    Rejected commit: Default

--- app/src/main/java/com/example/mahmo/nanoapp/MainActivity.java
+++ app/src/main/java/com/example/mahmo/nanoapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mahmo.nanoapp;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -12,13 +17,22 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.mahmo.nanoapp.Database.IDContract;
+import com.example.mahmo.nanoapp.Database.IDHelper;
+import com.example.mahmo.nanoapp.Database.TaskHelper;
 import com.example.mahmo.nanoapp.Event.Event;
 import com.example.mahmo.nanoapp.Event.Event_Fragment;
 import com.example.mahmo.nanoapp.Task.Task;
 import com.example.mahmo.nanoapp.Task.Task_Fragment;
 
 import java.util.ArrayList;
+import java.util.UUID;
+
+import static android.R.attr.x;
+import static android.R.id.edit;
+import static java.util.UUID.randomUUID;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -26,13 +40,14 @@ public class MainActivity extends AppCompatActivity
     public static int TaskId=1 ;
     public static int EventId=1 ;
     static boolean first = true;
-    public static boolean editable = false;
+    public static boolean editable;
     public static int editableTaskPosition;
     public static int editableEventPosition;
     public static ArrayList<Task> tasks;
     public static ArrayList<Event> events;
-
-Cursor cursor;
+    public Cursor cursor;
+    public ContentValues contentValues;
+    public IDHelper idHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,17 +57,25 @@ Cursor cursor;
         setSupportActionBar(toolbar);
         editableTaskPosition = 0;
         editableEventPosition = 0;
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
+        idHelper=new IDHelper(getApplicationContext());
+        cursor=idHelper.getData();
         if (first) {
+
             events = new ArrayList<Event>();
             tasks = new ArrayList<Task>();
-
             fragment_id = R.id.nav_Tasks;
             first = false;
+            if(!cursor.equals(null)) {
+                cursor.moveToFirst();
+                while (cursor.isAfterLast() == false) {
+
+                    TaskId=cursor.getInt(1);
+                    EventId=cursor.getInt(2);
+                    cursor.moveToNext();
+                }
+            }
         }
 
         navigationView.setCheckedItem(fragment_id);
@@ -65,7 +88,6 @@ Cursor cursor;
             android.app.FragmentManager fm = getFragmentManager();
             fm.beginTransaction().replace(R.id.content_fragment, new Event_Fragment()).commit();
         }
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -90,6 +112,7 @@ Cursor cursor;
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+
             super.onBackPressed();
         }
 
@@ -109,7 +132,6 @@ Cursor cursor;
             fragment_id = R.id.nav_Events;
             android.app.FragmentManager fm = getFragmentManager();
             fm.beginTransaction().replace(R.id.content_fragment, new Event_Fragment()).commit();
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -117,6 +139,13 @@ Cursor cursor;
         return true;
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        contentValues=new ContentValues();
+        contentValues.put(IDContract.IDEntry.TASK_ID,TaskId);
+        contentValues.put(IDContract.IDEntry.EVENT_ID,EventId);
+        idHelper.update(1,contentValues);
 
-
+    }
 }
